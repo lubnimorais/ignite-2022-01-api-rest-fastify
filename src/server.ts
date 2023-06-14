1f3f6da0f97a7654892a38e031f206022f1f5dd0
Lubni Morais <lubni.morais@gmail.com>
2023-06-14 12:09:15 -0300

    feat: Added lib zod and dotenv, creates file .env

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,33 @@
+import 'dotenv/config';
+
 import fastify from 'fastify';
+
+// import crypto from 'node:crypto';
+
 import { knex } from './database';
+import { env } from './env';
 
 const app = fastify();
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*');
+  // const transaction = await knex('transactions')
+  //   .insert({
+  //     id: crypto.randomUUID(),
+  //     title: 'Transação de teste',
+  //     amount: 1000,
+  //   })
+  //   .returning('*');
 
-  return tables;
+  const transactions = await knex('transactions')
+    .where('amount', 500)
+    .select('*');
+
+  return transactions;
 });
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('HTTP Server Running!');
